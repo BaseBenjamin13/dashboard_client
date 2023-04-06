c7a34c5f45fdc993ac83e9b60ffa146cc7af77fa
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-04-05 23:50:12 -0400

    navbar init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 
+import NavBar from './components/NavBar';
+
 function App() {
   return (
     <div className="App">
         <h1>Dashboard</h1>
+        <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/NavBar.css';
+
+function NavBar() {
+  return (
+    <div className="navbar">
+        <h1>Register</h1>
+        <h1>Login</h1>
+    </div>
+  )
+}
+
+export default NavBar
