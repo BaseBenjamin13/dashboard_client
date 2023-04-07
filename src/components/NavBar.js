552149ee94aa0f8964698f597fe7c154c890a512
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-04-06 21:37:01 -0400

    setting up register and login links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/NavBar.css';
 
 function NavBar() {
   return (
     <div className="navbar">
-        <h1>Register</h1>
-        <h1>Login</h1>
+        <Link to='/register' className="navbar-link"><h1>Register</h1></Link>
+        
+        <Link to='/login' className="navbar-link"><h1>Login</h1></Link>
     </div>
   )
 }
