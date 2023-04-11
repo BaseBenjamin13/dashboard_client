e4b817344431dffaf99d1de39684597709af6193
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-04-11 00:18:24 -0400

    setting up material ui theme in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 
 import './App.css';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import HomePage from './pages/HomePage';
 import Page404 from './pages/404';
 
 function App() {
-  return (
-    <div className="App">
-        <Routes>
-            <Route path="/" element={<HomePage />} />
+
+    const [theme, colorMode] = useMode();
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+
+                <div className="App">
+                    <main className="content">
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
 
 
-            <Route path="*" element={<Page404 />} />
-        </Routes>
-    </div>
-  );
+                        <Route path="*" element={<Page404 />} />
+                    </Routes>
+                    </main>
+                </div>
+
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
