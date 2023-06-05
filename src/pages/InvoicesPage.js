e8a1c82b2e268a9d0cfa43ab3bd521b0f17e1ded
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-06-05 00:34:17 -0400

    made getInvoices to get invoices by logged in user

--- src/pages/InvoicesPage.js
+++ src/pages/InvoicesPage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react';
+import axios from 'axios';
+
+import { UserContext } from '../contexts/UserContext';
 
 function InvoicesPage() {
-  return (
-    <div>InvoicesPage</div>
-  )
+
+    const { user, setUser } = useContext(UserContext);
+    const [invoices, setInvoices] = useState();
+
+    function getInvoices() {
+        if (user.ID) {
+            axios.get(`${process.env.REACT_APP_API_URL}invoices/${user.ID}/`)
+                .then((res) => {
+                    setInvoices(res.data);
+                    console.log(res);
+                })
+                .catch((err) => console.log(err));
+        }
+    }
+
+    useEffect(() => {
+        getInvoices()
+    }, [])
+
+    return (
+        <div>InvoicesPage</div>
+    )
 }
 
 export default InvoicesPage;
