0a35106de2d45b95a7f4d29b936612edc565ddb5
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-06-11 23:31:01 -0400

    displaying clients location by state in pie chart

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,13 +9,17 @@ import { Pie } from '@nivo/pie';
 function HomePage() {
 
     const { user, setUser } = useContext(UserContext);
-    const { clientLocations, setClientLocations } = useState();
+    const [ clientLocations, setClientLocations ] = useState();
 
     function getClientLocations() {
         if (user.ID) {
+            let tempClientLocations = [];
             axios.get(`${process.env.REACT_APP_API_URL}clients/${user.ID}/state/count`)
                 .then((res) => {
-                    setClientLocations(res.data.state_counts);
+                    res.data.state_counts.map((state) => {
+                        tempClientLocations.push({ id: state.state, value: state.count })
+                    })
+                    setClientLocations(tempClientLocations)
                 })
                 .catch((err) => console.log(err));
         }
@@ -31,63 +35,68 @@ function HomePage() {
 
             <div className='chart-container'>
                 <h2>Client Location</h2>
-                <Pie
-                    activeOuterRadiusOffset={8}
-                    animate
-                    data={[
-                        {
-                            color: 'hsl(22, 65%, 20%)',
-                            id: 'yoyo',
-                            value: 141
-                        },
-                        {
-                            color: 'hsl(284, 70%, 50%)',
-                            id: 'erlang',
-                            value: 284
-                        },
-                        {
-                            color: 'hsl(244, 70%, 50%)',
-                            id: 'haskell',
-                            value: 311
-                        },
-                        {
-                            color: 'hsl(352, 70%, 50%)',
-                            id: 'ruby',
-                            value: 510
-                        },
-                        {
-                            color: 'hsl(132, 80%, 50%)',
-                            id: 'elixir',
-                            value: 295
-                        },
-                        {
-                            color: 'hsl(149, 70%, 50%)',
-                            id: 'hack',
-                            value: 367
-                        },
-                        {
-                            color: 'hsl(71, 70%, 50%)',
-                            id: 'php',
-                            value: 71
-                        },
-                    ]}
-                    height={400}
-                    legends={[]}
-                    margin={{
-                        bottom: 30,
-                        left: 20,
-                        right: 20,
-                        top: 30
-                    }}
-                    theme={{
-                        fontColor: 'white',
-                        fontSize: 24,
-                        text: {
-                            fontFamily: '\'SFMono-Regular\', Consolas, \'Liberation Mono\', Menlo, Courier, monospace'
-                        }
-                    }}
-                    width={700}
-                />
+                {
+                    clientLocations ?
+                        < Pie
+                            activeOuterRadiusOffset={8}
+                            animate
+                            // data={[
+                            //     {
+                            //         color: 'hsl(22, 65%, 20%)',
+                            //         id: 'yoyo',
+                            //         value: 141
+                            //     },
+                            //     {
+                            //         color: 'hsl(284, 70%, 50%)',
+                            //         id: 'erlang',
+                            //         value: 284
+                            //     },
+                            //     {
+                            //         color: 'hsl(244, 70%, 50%)',
+                            //         id: 'haskell',
+                            //         value: 311
+                            //     },
+                            //     {
+                            //         color: 'hsl(352, 70%, 50%)',
+                            //         id: 'ruby',
+                            //         value: 510
+                            //     },
+                            //     {
+                            //         color: 'hsl(132, 80%, 50%)',
+                            //         id: 'elixir',
+                            //         value: 295
+                            //     },
+                            //     {
+                            //         color: 'hsl(149, 70%, 50%)',
+                            //         id: 'hack',
+                            //         value: 367
+                            //     },
+                            //     {
+                            //         color: 'hsl(71, 70%, 50%)',
+                            //         id: 'php',
+                            //         value: 71
+                            //     },
+                            // ]}
+                            data={clientLocations}
+                            height={400}
+                            legends={[]}
+                            margin={{
+                                bottom: 30,
+                                left: 20,
+                                right: 20,
+                                top: 30
+                            }}
+                            theme={{
+                                fontColor: 'white',
+                                fontSize: 28,
+                                text: {
+                                    fontFamily: '\'SFMono-Regular\', Consolas, \'Liberation Mono\', Menlo, Courier, monospace'
+                                }
+                            }}
+                            width={700}
+                        />
+                        : <h1>Loading...</h1>
+                }
             </div>
 
         </div>
