1358358c288c67f262a3317378d1604b095a2884
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-06-15 00:57:46 -0400

    making invoice paid & unpaid chart

---
+++ src/components/charts/InvoicesCountChart.js
@@ -0,0 +1,62 @@
+import React, { useState, useContext, useEffect } from 'react';
+import axios from 'axios';
+import '../../styles/HomePage.css'
+
+import { Chart } from 'react-basic-charts';
+import 'react-basic-charts/index.css';
+
+function InvoicesCountChart({ userID }) {
+
+    const [invoiceUnpaidCount, setInvoiceUnpaidCount] = useState();
+    const [invoicePaidCount, setInvoicePaidCount] = useState();
+
+    function getInvoiceCounts() {
+        if (userID) {
+            axios.get(`${process.env.REACT_APP_API_URL}invoices/${userID}/unpaid/count`)
+                .then((res) => {
+                    console.log(res.data.count)
+                    setInvoiceUnpaidCount(res.data.count)
+                })
+                .catch((err) => console.log(err));
+            axios.get(`${process.env.REACT_APP_API_URL}invoices/${userID}/paid/count`)
+                .then((res) => {
+                    console.log(res.data.count)
+                    setInvoicePaidCount(res.data.count)
+                })
+                .catch((err) => console.log(err));
+        }
+    }
+
+
+    useEffect(() => {
+        getInvoiceCounts();
+    }, [])
+
+    return (
+        <div className='chart-container'>
+            <h2>Invoices Paid & Unpaid</h2>
+            <div className='chart'>
+                {
+                    invoicePaidCount && invoiceUnpaidCount ?
+                        <Chart
+                            height={300}
+                            colors={['#A6E7DB', '#2D7A7A', '#76C7D2']}
+                            data={{
+                                Unpaid: [
+                                    invoiceUnpaidCount
+                                ],
+                                Paid: [
+                                    invoicePaidCount
+                                ]
+                            }}
+                            displayValue={(value) =>  value}
+                        />
+                        : <h1>Loading...</h1>
+                }
+            </div>
+        </div>
+    )
+}
+
+
+export default InvoicesCountChart;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,17 +4,19 @@ import { UserContext } from '../contexts/UserContext';
 import '../styles/HomePage.css'
 
 import ClientLocationChart from '../components/charts/ClientLocationChart';
+import InvoicesCountChart from '../components/charts/InvoicesCountChart';
 
 function HomePage() {
 
     const { user, setUser } = useContext(UserContext);
-
 
     return (
         <div>
             <h1>Home Page</h1>
 
             <ClientLocationChart userID={user.ID} />
+
+            <InvoicesCountChart userID={user.ID} />
 
         </div>
     )
