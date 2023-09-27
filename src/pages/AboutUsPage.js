e527a1659b14a79113e473e10a1b53da5495816d
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2023-09-27 16:43:14 -0400

    made about us page mobile responsive

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,25 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { tokens, ColorModeContext } from "../theme";
+import { getIsMobile } from '../helpers/getIsMobile';
 
 function AboutUsPage() {
 
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        getIsMobile(setIsMobile)
+    }, [])
+
+    let boxStyle = isMobile ? {
+        backgroundColor: `${colors.primary[400]} !important`,
+        width: '80vw',
+        margin: '0 auto',
+        marginTop: '10vh',
+        padding: '10px',
+        borderRadius: '40px',
+        border: `5px solid ${colors.greenAccent[500]}`,
+    } : {
+        backgroundColor: `${colors.primary[400]} !important`,
+        width: '50vw',
+        margin: '0 auto',
+        marginTop: '20vh',
+        padding: '20px',
+        borderRadius: '40px',
+        border: `5px solid ${colors.greenAccent[500]}`,
+    }
 
     return (
         <div>
             <Box
-                sx={{
-                    backgroundColor: `${colors.primary[400]} !important`,
-                    width: '50vw',
-                    margin: '0 auto',
-                    marginTop: '20vh',
-                    padding: '20px',
-                    borderRadius: '40px',
-                    border: `5px solid ${colors.greenAccent[500]}`,
-                }}
+                sx={boxStyle}
             >
                 <Typography variant="h2" color={colors.greenAccent[500]}>
                     About Us
